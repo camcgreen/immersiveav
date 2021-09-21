c8c36bfa6b84713b852ebf0314202eb04404117d
cameron-green-1 <cameron.green@voxelstudio.co.uk>
2021-09-21 09:51:49 +0100

    remove full stop after case studies

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -3,7 +3,7 @@ export const breakText = [
     h1: "We're ImmersiveAV",
     p: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n Aliquam vel neque et urna fermentum vestibulum condimentum a mi.",
   },
-  { h1: "Case Studies." },
+  { h1: "Case Studies" },
   { h1: "News" },
   {
     h1: "ImmersiveAV Expo & Hire",
