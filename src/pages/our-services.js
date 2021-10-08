69b5ad4b6ab2e45d43cefedc55a5c129a285e62b
FA-Chirinos-Lopez <felixchirinoslopez@gmail.com>
2021-10-08 16:33:09 +0100

    08/10/2021
    our-services structure and scss
    sub-footer as element with own scss
    .svg files for background of our-services page

--- src/pages/our-services.js
+++ src/pages/our-services.js
@@ -1,5 +1,67 @@
-import * as React from "react"
+import * as React from "react";
+import Header from "../components/header";
+import Phone from "../components/phone";
+import Footer from"../components/footer";
+import HireExpo from "../components/hire-expo";
+import Hero from "../components/hero";
+import SubFooter from "../components/subFooter";
+import "../styles/our-services.scss"
+import { StaticImage } from "gatsby-plugin-image";
 
-const OurServices = () => <p>Our Services (in development)</p>
 
+/*<StaticImage
+            className="servicesFirstSectionBackground"
+            src={LogoOnlyBlue}
+            alt=""/>
+            <Header/>
+    <Phone/>
+    <Hero/>
+            
+     <HireExpo/>
+        <Footer/>
+       <SubFooter/>        
+            
+            
+            
+            
+            
+            */
+//<img src={IconPhone} alt="" />
+//<img src={LogoOnlyBlue} alt="" />
+const firstTextServices="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in sapien ac nunc facilisis scelerisque in in ex. Donec dapibus, nunc egestas";
+const secondTextServices="";
+const OurServices = () =>{
+
+return(
+<div>
+<Header/>
+<Phone/>
+<Hero/>
+<main className="mainServices">
+     <article className="servicesSections">
+        <div className="firstTitleBox"><h1 className="firstTitleBoxText">Services</h1></div>
+            <div>
+                <title><h2 className="firstTextServices">{firstTextServices}</h2></title>
+            </div>
+            <div>
+            </div>        
+    </article>
+    <article className="servicesSecondSection">
+        <div className="servicesSecondTitleBox"><h1 className="servicesSecondTitleBoxText">Services</h1></div>
+            <div>
+                <title><h2 className="firstTextServices">{secondTextServices}</h2></title>
+            </div>
+            <div>
+            </div>        
+    </article>
+
+
+           
+</main>
+        <HireExpo/>
+        <Footer/>
+       <SubFooter/>        
+       </div>
+    )
+}
 export default OurServices
