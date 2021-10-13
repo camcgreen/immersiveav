fdbc464a5a24f64ca1374a2708962c6c669f2d47
FA-Chirinos-Lopez <felixchirinoslopez@gmail.com>
2021-10-13 12:38:54 +0100

    img's and styles
    images and styles improved

--- src/pages/our-services.js
+++ src/pages/our-services.js
@@ -32,7 +32,7 @@ import News from "../components/news";
 const firstSectionTexts=[
     
     'Lorem ipsum dolor sit amet consectetur adipiscing elit',
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\n\n\n quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'Lorem ipsum dolor sit amet consectetur adipiscing elit',
 
 ]
@@ -58,18 +58,18 @@ return(
                     
                     <div className="firstSection1images">
                         <div className="flexBox1_1">
-                        <StaticImage className="img1_1" src="..\images\u9.JPG" alt=""></StaticImage>
-                        <StaticImage className="emptySpace1_0" src="..\images\u9.JPG" alt=""></StaticImage>
-                        <StaticImage className="img1_2" src="..\images\u10.JPG" alt=""></StaticImage>
-                        <StaticImage className="emptySpace1_1" src="..\images\u9.JPG" alt=""></StaticImage>
+                        <StaticImage className="littleImg img1_1" src="..\images\u9.JPG" alt=""></StaticImage>
+                        <div className="emptySpace emptySpace1_0"></div>
+                        <StaticImage className="littleImg img1_2" src="..\images\u10.JPG" alt=""></StaticImage>
+                        <div className="emptySpace emptySpace1_1" ></div>
                         
                         </div>
                         <div className="flexBox1_2">
-                        <StaticImage className="img1_3" src="..\images\u8.jpg" alt=""></StaticImage>
-                        <StaticImage className="img1_4" src="..\images\c11.jpg" alt=""></StaticImage>
-                        <StaticImage className="emptySpace2_0" src="..\images\u9.JPG" alt=""></StaticImage>                        
-                        <StaticImage className="img1_5" src="..\images\u4.jpg" alt=""></StaticImage>
-                        <StaticImage className="emptySpace2_1" src="..\images\u9.JPG" alt=""></StaticImage>
+                        <StaticImage className="littleImg img1_3" src="..\images\u8.jpg" alt=""></StaticImage>
+                        <StaticImage className="littleImg img1_4" src="..\images\c11.jpg" alt=""></StaticImage>
+                        <div className="emptySpace emptySpace2_0"></div>                        
+                        <StaticImage className="littleImg img1_5" src="..\images\u4.jpg" alt=""></StaticImage>
+                        <div className="emptySpace emptySpace2_1"></div>
                         </div>   
                     </div>
                     </div>
@@ -79,7 +79,7 @@ return(
                             <div className="flexBox_1_2_1P">{firstSectionTexts[1]}</div>
                         </div>
                         <div className="flexBox_1_2_2">
-                        <StaticImage className="img_1_2_2_0" src="..\images\u12.JPEG" alt=""></StaticImage>
+                        <StaticImage className="littleImg img_1_2_2_0" src="..\images\u12.JPEG" alt=""></StaticImage>
                         <StaticImage className="img_1_2_2_1" src="..\images\u3.JPEG" alt=""></StaticImage>
                         </div>
 
@@ -101,7 +101,7 @@ return(
             </div>
             <div className="flexBox_2_2">
             <StaticImage className="img_2_2_1" src="..\images\u8.JPG" alt=""></StaticImage>
-            <StaticImage className="img_2_2_2" src="..\images\u5.JPG" alt=""></StaticImage>
+            <StaticImage className="littleImg img_2_2_2" src="..\images\u5.JPG" alt=""></StaticImage>
             </div>
         </div>        
     </article>
@@ -133,7 +133,7 @@ return(
                 <div className="text_4_1_2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris'<br /><br />' nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
             </div>
             <div className="flexBox_4_2">
-            <StaticImage className="img_4_2_1" src="..\images\u11.jpg"  alt=""></StaticImage>
+            <StaticImage className="littleImg img_4_2_1" src="..\images\u11.jpg"  alt=""></StaticImage>
             <StaticImage className="img_4_2_2" src="..\images\u2.jpg" alt=""></StaticImage>
             </div>
         </div>        
